a73fb616ebc05a0ff2d839a9397e45c0de3688b8
saiddlkc <saidalkilic@gmail.com>
2024-08-06 15:57:34 +0200

    nodemon

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -23,12 +22,12 @@ userSchema.statics.signup = async function (email, password) {
   if (!email || !password) {
     throw Error("All Fields must be filled");
   }
-  if (!validator.isEmail(email)) {
-    throw Error("Email is not valid");
-  }
-  if (!validator.isStrongPassword(password)) {
-    throw Error("Password not strong enough");
-  }
+  // if (email) {
+  //   throw Error("Email is not valid");
+  // }
+  // if (password) {
+  //   throw Error("Password not strong enough");
+  // }
 
   const exists = await this.findOne({ email });
   if (exists) {
